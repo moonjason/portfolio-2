fef5bb3263aab4a60e28f78d2c54ae8cdc563b7c
Jason Moon <jasonmoonjm@gmail.com>
2020-02-15 17:17:07 -0800

    set up profile card, add images

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            This is the header
+  return (
+    <div class="wrapper">
+      <div class="profile-card js-profile-card">
+        <div id="img" class="profile-card__img">
+          <img
+            src="#"
+            alt="profile card"
+          />
         </div>
-    )
-} 
+
+        <div class="profile-card__cnt js-profile-cnt">
+          <div class="profile-card__name">Jason Moon</div>
+          <div class="profile-card__txt">
+            Software Developer
+          </div>
+          <div class="profile-card-loc">
+            <span class="profile-card-loc__icon"></span>
+
+            <span class="profile-card-loc__txt">Los Angeles, California</span>
+          </div>
+          {/* <div class="profile-card-inf">
+            <div class="profile-card-inf__item">
+              <div class="profile-card-inf__title">1598</div>
+              <div class="profile-card-inf__txt">Followers</div>
+            </div>
+
+            <div class="profile-card-inf__item">
+              <div class="profile-card-inf__title">65</div>
+              <div class="profile-card-inf__txt">Following</div>
+            </div>
+
+            <div class="profile-card-inf__item">
+              <div class="profile-card-inf__title">123</div>
+              <div class="profile-card-inf__txt">Articles</div>
+            </div>
+
+            <div class="profile-card-inf__item">
+              <div class="profile-card-inf__title">85</div>
+              <div class="profile-card-inf__txt">Works</div>
+            </div>
+          </div> */}
+
+          <div class="profile-card-social">
+            <img class="icon" src="img/icons8-github-48.png"/>
+            <img class="icon" src="img/icons8-linkedin-48.png"/>
+            <img class="icon" src="img/icons8-mailing-48.png"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
