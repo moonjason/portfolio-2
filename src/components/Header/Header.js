389d4d4a39a0299d4928ab5b7e54b95cc27d51e4
Admin <admin@Admins-iMac.local>
2020-02-25 14:30:04 -0800

    add basic hover effect

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [currentLink, setCurrentLink] = useState("");
+
   return (
     <div class="wrapper">
       <div class="profile-card js-profile-card">
@@ -13,6 +15,7 @@ const Header = () => {
 
         <div class="profile-card__cnt js-profile-cnt">
           <div class="profile-card__name">Jason Moon</div>
+          <div class="profile-card__line"></div>
           <div class="profile-card__txt">Software Developer <span class="text-spacing"></span> Los Angeles</div>
           <div class="profile-card-loc">
             <span class="profile-card-loc__icon"></span>
@@ -42,11 +45,12 @@ const Header = () => {
           </div> */}
 
           <div class="profile-card-social">
-            <a href="https://www.github.com/moonjason"><img class="icon" src="img/icons8-github-48.png" alt="github-link"/></a>
-            <a href="https://www.linkedin.com/in/jasonmoon96"><img class="icon" src="img/icons8-linkedin-48.png" alt="linkedin-link"/></a>
-            <a href="mailto:jasonmoonjm@gmail.com"><img class="icon" src="img/icons8-mailing-48.png" alt="email-link"/></a>
-            <a href="https://docs.google.com/document/d/1JUKV8INNshnN7zcbX3-JNoPpL8UUpIV8OXVA-xIU29A/edit?usp=sharing"><img class="icon" src="img/icons8-download-resume-48.png" alt="resume-link"/></a>
+            <a href="https://www.github.com/moonjason" onMouseEnter={() => setCurrentLink("Github: moonjason")} onMouseLeave={() => setCurrentLink("")}><img class="icon" src="img/icons8-github-48.png" alt="github-link"/></a>
+            <a href="https://www.linkedin.com/in/jasonmoon96" onMouseEnter={() => setCurrentLink("LinkedIn: /in/jasonmoon96")} onMouseLeave={() => setCurrentLink("")}><img class="icon" src="img/icons8-linkedin-48.png" alt="linkedin-link"/></a>
+            <a href="mailto:jasonmoonjm@gmail.com" onMouseEnter={() => setCurrentLink("Email: jasonmoonjm@gmail.com")} onMouseLeave={() => setCurrentLink("")}><img class="icon" src="img/icons8-mailing-48.png" alt="email-link"/></a>
+            <a href="https://docs.google.com/document/d/1JUKV8INNshnN7zcbX3-JNoPpL8UUpIV8OXVA-xIU29A/edit?usp=sharing" onMouseEnter={() => setCurrentLink("Resume")} onMouseLeave={() => setCurrentLink("")}><img class="icon" src="img/icons8-download-resume-48.png" alt="resume-link"/></a>
           </div>
+        <p class="link-desc">{currentLink}</p>
         </div>
       </div>
     </div>
